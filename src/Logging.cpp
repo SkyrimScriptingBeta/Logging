0f068e95f809fc6089ff5b5e7a4c20aa7ced83f6
Mrowr Purr <mrowr.purr@gmail.com>
2025-03-19 19:19:54 -0700

    Simplifying (note: only NG needs this library)

--- src/Logging.cpp
+++ src/Logging.cpp
@@ -1,98 +1,52 @@
-// Include spdlog support for a basic file logger
-#include <spdlog/sinks/basic_file_sink.h>
+#include "SkyrimScripting/Logging.h"
 
-// TODO remove
-#include <RE/Skyrim.h>
-
-// Allows us to check if a debugger is attached (optional, see below)
 #include <SKSE/SKSE.h>
-#include <SKSEPluginInfo.h>
 #include <Windows.h>
+#include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/sinks/msvc_sink.h>
 
-#include <atomic>
-#include <filesystem>
 #include <format>
 
-namespace SkyrimScripting::Logging::Internal {
-    std::atomic<bool> IsLoggerInitialized;
+namespace SkyrimScripting::Logging {
 
-    void InitializeLogger() {
-        if (IsLoggerInitialized.exchange(true)) return;
-
-        // return;  // Disable for now!
-
-        // Get the logs folder
-        auto logsFolder = SKSE::log::log_directory();
-        if (!logsFolder) {
-            SKSE::stl::report_and_fail("SKSE log_directory not provided, logs disabled.");
-            return;
-        }
-
-        // Get the plugin name
-#ifdef SKSE_PLUGIN_NAME
-        auto* pluginName = SKSE_PLUGIN_NAME;
-#elif __has_include(<SKSEPluginInfo.h>)
-        auto* pluginName = SKSEPluginInfo::GetPluginName();
-#else
-    #error \
-        "SKSEPluginInfo.h not found and SKSE_PLUGIN_NAME not defined. To use SkyrimScripting.Logging, please either provide a log file name or a log folder path OR define SKSE_PLUGIN_NAME"
-#endif
-
-        // Construct the log path
-        auto logPath = *logsFolder / std::format("{}.log", pluginName);
-
-        // Create the file logger
-        auto fileLoggerPtr =
-            std::make_shared<spdlog::sinks::basic_file_sink_mt>(logPath.string(), true);
-
-        // Create the spdlog logger pointer
-        //
-        // Minor known issue: if logs directory was for some reason not created,
-        // this code does not run so the debugger logger is not created in that case
-        std::shared_ptr<spdlog::logger> loggerPtr;
-        if (IsDebuggerPresent()) {
-            auto debugLoggerPtr = std::make_shared<spdlog::sinks::msvc_sink_mt>();
-            spdlog::sinks_init_list loggers{std::move(fileLoggerPtr), std::move(debugLoggerPtr)};
-            loggerPtr = std::make_shared<spdlog::logger>("log", loggers);
-        } else {
-            loggerPtr = std::make_shared<spdlog::logger>("log", std::move(fileLoggerPtr));
-        }
-
-        // Setup the logger
-        spdlog::set_default_logger(std::move(loggerPtr));
-
-        // The default level is the lowest level, trace
-        spdlog::set_level(spdlog::level::trace);
-        spdlog::flush_on(spdlog::level::trace);
-
-        // Use the default pattern
-        //
-        // You can override with something like:
-        // spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%l] %v");
+    void InitializeLogger(std::string_view pluginName) {}
+    // Get the logs folder
+    auto logsFolder = SKSE::log::log_directory();
+    if (!logsFolder) {
+        SKSE::stl::report_and_fail("SKSE log_directory not provided, logs disabled.");
+        return;
     }
-}
 
-// Did we build with Entrypoint support built-in?
-//
-// If so, go ahead and register the logger initialization
-// to run as the VERY FIRST thing in the plugin
-// #if __has_include(<SkyrimScripting/Entrypoint.h>)
-#include <SkyrimScripting/Entrypoint.h>
+    // Construct the log path
+    auto logPath = *logsFolder / std::format("{}.log", pluginName);
 
-SKSEPlugin_Entrypoint_PreInit_HighPriority {
-    SkyrimScripting::Logging::Internal::InitializeLogger();
-    SKSE::log::info("INITIALIZED LOGGER");
-    return true;
-}
+    // Create the file logger
+    auto fileLoggerPtr =
+        std::make_shared<spdlog::sinks::basic_file_sink_mt>(logPath.string(), true);
 
-SKSEPlugin_Entrypoint {
-    SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message* a_msg) {
-        if (a_msg->type == SKSE::MessagingInterface::kDataLoaded) {
-            if (auto* consoleLog = RE::ConsoleLog::GetSingleton()) {
-                consoleLog->Print("Hello from SkyrimScripting.Logging ENTRYPOINT!");
-            }
-        }
-    });
+    // Create the spdlog logger pointer
+    //
+    // Minor known issue: if logs directory was for some reason not created,
+    // this code does not run so the debugger logger is not created in that case
+    std::shared_ptr<spdlog::logger> loggerPtr;
+    if (IsDebuggerPresent()) {
+        auto                    debugLoggerPtr = std::make_shared<spdlog::sinks::msvc_sink_mt>();
+        spdlog::sinks_init_list loggers{std::move(fileLoggerPtr), std::move(debugLoggerPtr)};
+        loggerPtr = std::make_shared<spdlog::logger>("log", loggers);
+    } else {
+        loggerPtr = std::make_shared<spdlog::logger>("log", std::move(fileLoggerPtr));
+    }
+
+    // Setup the logger
+    spdlog::set_default_logger(std::move(loggerPtr));
+
+    // The default level is the lowest level, trace
+    spdlog::set_level(spdlog::level::trace);
+    spdlog::flush_on(spdlog::level::trace);
+
+    // Use the default pattern
+    //
+    // You can override with something like:
+    // spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%l] %v");
 }
-// #endif
+}
--- xmake.lua
+++ xmake.lua
@@ -3,14 +3,10 @@ add_rules("mode.debug", "mode.release", "mode.releasedbg")
 set_languages("c++23")
 
 option("commonlib")
-    set_default("skyrim-commonlib-ae")
+    set_default("skyrim-commonlib-ng")
 option_end()
 
 option("build_example")
-    set_default(true)
-option_end()
-
-option("use_skyrimscripting_entrypoint")
     set_default(true)
 option_end()
 
@@ -18,46 +14,37 @@ library_name = "SkyrimScripting.Logging"
 
 if has_config("build_example") then
     add_repositories("SkyrimScripting https://github.com/SkyrimScripting/Packages.git")
-    add_repositories("SkyrimScriptingBeta https://github.com/SkyrimScriptingBeta/Packages.git")
-    add_repositories("MrowrLib https://github.com/MrowrLib/Packages.git")
 end
-
--- Example SKSE plugin using the static library
-mod_info = {
-    name = "Test plugin for " .. library_name,
-    version = "0.0.1",
-    author = "Mrowr Purr",
-    email = "mrowr.purr@gmail.com"
-}
-
--- Available versions: ae, se, ng, vr
-skyrim_version = "ae"
 
 add_requires(get_config("commonlib"))
-add_requires("skse_plugin_info")
 
-if has_config("use_skyrimscripting_entrypoint") then
-    add_requires("SkyrimScripting.Entrypoint", { configs = { commonlib = get_config("commonlib") }})
-end
+target(library_name)
+    set_kind("static")
+    add_files("src/*.cpp")
+    add_includedirs("include", { public = true })
+    add_headerfiles("include/(**.h)")
+    add_packages(get_config("commonlib"), { public = true })
 
--- if has_config("build_example") then
---     add_requires("skyrim-commonlib-" .. skyrim_version)
---     add_requires("SkyrimScripting.Entrypoint", { configs = { commonlib = "skyrim-commonlib-" .. skyrim_version }})
--- end
-
--- if has_config("commonlib") then
--- target(library_name)
---     set_kind("static")
---     add_files("src/*.cpp")
---     add_packages(get_config("commonlib"), { public = true })
---     add_packages("skse_plugin_info", { public = true })
---     add_packages("SkyrimScripting.Entrypoint", { public = true })
---     -- if has_config("use_skyrimscripting_entrypoint") then
---     --     add_packages("SkyrimScripting.Entrypoint", { public = true })
---     -- end
--- target_end()
--- end
+commonlib_version = get_config("commonlib"):match("skyrim%-commonlib%-(.*)")
 
 if has_config("build_example") then
-    includes("skse.lua")
+    mod_info = { name = "Test plugin for " .. library_name }
+
+    mods_folders = os.getenv("SKYRIM_MODS_FOLDERS")
+
+    if mods_folders then
+        mod_info.mods_folders = mods_folders
+    else
+        print("SKYRIM_MODS_FOLDERS environment variable not set")
+    end
+
+    target("_SksePlugin-" .. commonlib_version:upper())
+        set_basename(mod_info.name .. "-" .. commonlib_version:upper())
+        add_files("*.cpp")
+        add_rules("@skyrim-commonlib-" .. commonlib_version .. "/plugin", {
+            mod_name = mod_info.name .. " (" .. commonlib_version:upper() .. ")",
+            mods_folders = mod_info.mods_folders or "",
+            name = mod_info.name
+        })
+        add_deps(library_name)
 end
