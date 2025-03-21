bdff3e453e27c04302f4552e06d9a7b1c1f703de
Mrowr Purr <mrowr.purr@gmail.com>
2025-03-20 19:48:38 -0700

    Log() --> SKSE::log::info()

--- include/SkyrimScripting/Logging.h
+++ include/SkyrimScripting/Logging.h
@@ -2,4 +2,5 @@
 
 #include "Logging/ConsoleLog.h"        // IWYU pragma: export
 #include "Logging/InitializeLogger.h"  // IWYU pragma: export
+#include "Logging/Log.h"               // IWYU pragma: export
 #include "Logging/PrintToConsole.h"    // IWYU pragma: export
---
+++ include/SkyrimScripting/Logging/Log.h
@@ -0,0 +1,4 @@
+#pragma once
+
+// Just a really super duper simple logging macro for ::info specifically
+#define Log(...) SKSE::log::info(__VA_ARGS__)
